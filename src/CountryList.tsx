bda90df3676140772434cf5ca7163554dc80e790
arminsalcin1 <armin.salcin1@gmail.com>
2021-02-10 16:45:56 +0100

    fix one flag

--- src/CountryList.tsx
+++ src/CountryList.tsx
@@ -123,11 +123,7 @@ const CountryItem = (props: CountryItemProps) => {
           />
         )}
         <View style={styles.itemCountryName}>
-          <CountryText
-            allowFontScaling={false}
-            numberOfLines={2}
-            ellipsizeMode='tail'
-          >
+          <CountryText numberOfLines={2} ellipsizeMode='tail'>
             {country.name}
             {extraContent.length > 0 && ` (${extraContent.join(', ')})`}
           </CountryText>
